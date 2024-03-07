9a710aa117cfd279d9388ccb66c2969e7f4395c1
kieranwatt <kieranmwatt@gmail.com>
2024-03-07 09:25:29 -0500

    Update BarChart.vue

--- nycopendata-project/src/views/BarChart.vue
+++ nycopendata-project/src/views/BarChart.vue
@@ -5,6 +5,19 @@
 </template>
 
 <script setup>
+import { ref, onBeforeMount } from "vue";
+const squirrels = ref("");
+async function getSquirrels() {
+  let res = await fetch("https://data.cityofnewyork.us/resource/vfnx-vebw.json");
+  let data = await res.json();
+  squirrels.value = data.results;
+  console.log(squirrels.value);
+}
+
+onBeforeMount(() => {
+  getSquirrels();
+});
+
 
 </script>
 

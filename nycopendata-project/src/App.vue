0e85f85d28154ad6544fd4b61bc54ec592af42b9
DanielL492 <lindan18351@gmail.com>
2024-03-07 08:45:53 -0500

    zsd

--- nycopendata-project/src/App.vue
+++ nycopendata-project/src/App.vue
@@ -1,13 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-
 </script>
 
 <template>
   <header>
-
     <div class="wrapper">
-
       <nav>
         <RouterLink to="/">Bar Chart</RouterLink>
         <RouterLink to="/pie">Pie Chart</RouterLink>

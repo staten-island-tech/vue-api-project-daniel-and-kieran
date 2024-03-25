0d455f0e4a5afb1d032dbf3581064e806c0c65c2
DanielL492 <lindan18351@gmail.com>
2024-03-25 09:21:33 -0400

    sd

--- nycopendata-project/src/views/BarChart.vue
+++ nycopendata-project/src/views/BarChart.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <h1>This is a Bar Chart</h1>
-    <PieChart/>
   </div>
 </template>
 
--- nycopendata-project/src/views/PieChart.vue
+++ nycopendata-project/src/views/PieChart.vue
@@ -9,13 +9,22 @@
 import { ref, onBeforeMount } from 'vue';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
 import { Pie } from 'vue-chartjs'
-import * as chartConfig from './chartConfig.js'
+
+ChartJS.register(ArcElement, Tooltip, Legend)
 
 const loaded = ref(false);
 const chartData = ref({});
 
-async function gettrees() {
+onBeforeMount(() => {
   loaded.value = false;
+  try {
+    gettrees()
+  } catch (error) {
+    console.warn(error)
+  }
+});
+
+async function gettrees() {
   let data;
   try {
     let res = await fetch('https://data.cityofnewyork.us/resource/uvpi-gqnh.json');
@@ -24,23 +33,43 @@ async function gettrees() {
   } catch (e) {
     console.error(e);
   }
-  chartData.value = data;
+  const trees = [];
+  chartData.value.labels = [];
+  for (let tree of data) {
+    if (!chartData.value.labels.includes(tree.spc_common)) {
+      chartData.value.labels.push(tree.spc_common);
+    }
+    trees.push(tree.spc_common);
+  }
+
+  for (let bobs of chartData.value.labels) {
+    for (let tree in trees) {
+      if (tree == bobs) {
+        data++;
+      }
+    }
+  }
+
   console.log(chartData)
   loaded.value=true;
 }
-onBeforeMount(() => {
-  try {
-    gettrees()
-  } catch (error) {
-    console.warn(error)
-  }
-})
 
-ChartJS.register(ArcElement, Tooltip, Legend)
+/*
+1. for each tree species, add a label - if label is already in label array, dont push it
+2. for each tree species, also increment a counter for the tree species by 1
+3. for each tree species, push the number to data array
+*/
+
+/*const chartData = {
+  labels: ["a", "b"],
+  datasets: [{
+    backgroundColor: ["#ffffff", "#000000"],
+    data: [1, 2]
+  }]
+}*/
 
 const options = {
   responsive: true,
-  maintainAspectRatio: false
 }
 
 </script>

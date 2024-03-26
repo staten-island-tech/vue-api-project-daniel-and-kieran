7c1ecb3fee18c2a8e0db992e5577fb3357efbba8
kieranwatt <kieranmwatt@gmail.com>
2024-03-25 23:02:15 -0400

    just some tests

--- nycopendata-project/src/views/BarChart.vue
+++ nycopendata-project/src/views/BarChart.vue
@@ -1,11 +1,84 @@
-<template>
-  <div>
-    <h1>This is a Bar Chart</h1>
-    <Bar :data="data" :options="options" />
-  </div>
-</template>
-
 <script setup>
+/* import { ref, onBeforeMount } from 'vue';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { Pie } from 'vue-chartjs'
+
+ChartJS.register(ArcElement, Tooltip, Legend)
+
+const loaded = ref(false);
+const chartData = ref({
+  labels: [],
+  datasets: [{
+    data: [],
+    backgroundColor: [
+      'red',
+      'blue',
+      'green',
+    ]
+  }]
+});
+
+onBeforeMount(() => {
+  loaded.value = false;
+  try {
+    getTrees();
+  } catch (error) {
+    console.warn(error);
+  }
+});
+
+async function getTrees() {
+  let data;
+  try {
+    let res = await fetch('https://data.cityofnewyork.us/resource/uvpi-gqnh.json');
+    data = await res.json();
+    console.log(data);
+
+    const treeCounts = {};
+    for (let tree of data) {
+      if (!treeCounts[tree.spc_common]) {
+        treeCounts[tree.spc_common] = 1;
+      } else {
+        treeCounts[tree.spc_common]++;
+      }
+    }
+
+    for (let species in treeCounts) {
+      chartData.value.labels.push(species);
+      chartData.value.datasets[0].data.push(treeCounts[species]);
+    }
+
+    console.log(chartData);
+    loaded.value = true;
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+const options = {
+  responsive: true,
+};
+
+ */
+</script>
+
+<style scoped></style>
+/*
+1. for each tree species, add a label - if label is already in label array, dont push it
+2. for each tree species, also increment a counter for the tree species by 1
+3. for each tree species, push the number to data array
+*/
+
+/*const chartData = {
+  labels: ["a", "b"],
+  datasets: [{
+    backgroundColor: ["#ffffff", "#000000"],
+    data: [1, 2]
+  }]
+}*/
+
+
+
 
 //Chart js bs
 //APPLY CHANGES WE FIXED IN PIE CHART TO HERE
@@ -50,6 +123,3 @@
 //   }
 // })
 //bar chart of different type of species
-</script>
-
-<style scoped></style>

27cf58577a2b391801eca03e6302f228370ee264
kieranwatt <kieranmwatt@gmail.com>
2024-03-31 14:08:27 -0400

    Added main page

--- nycopendata-project/src/App.vue
+++ nycopendata-project/src/App.vue
@@ -1,18 +1,55 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+
+import { ref, onBeforeMount } from 'vue';
+
+const loaded = ref(false);
+const rawData = ref([]);
+
+onBeforeMount(() => {
+  loaded.value = false;
+  try {
+    getTrees();
+  } catch (error) {
+    console.warn(error);
+  }
+});
+
+async function getTrees() {
+  try {
+    const res = await fetch('https://data.cityofnewyork.us/resource/uvpi-gqnh.json');
+    rawData.value = await res.json();
+    console.log(rawData.value);
+    loaded.value = true;
+  } catch (error) {
+    console.error(error);
+  }
+}
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Bar Chart</RouterLink>
-        <RouterLink to="/pie">Pie Chart</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <div>
+    <header>
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/raw-data">Raw Data</RouterLink>
+          <RouterLink to="/">Bar Chart</RouterLink>
+          <RouterLink to="/pie">Pie Chart</RouterLink>
+        </nav>
+      </div>
+    </header>
 
-  <RouterView />
+    <RouterView />
+
+    <div v-if="loaded">
+      <h1>This is the Raw Data</h1>
+      <ul>
+        <li v-for="(tree, index) in rawData" :key="index">
+          {{ tree.spc_common }} - {{ tree.tree_id }} - {{ tree.boroname }}
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <style scoped>

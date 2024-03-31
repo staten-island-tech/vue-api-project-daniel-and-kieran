270d45a3865b2842162f886f22cbe81262eaa6a8
DannyL1234 <144807703+DanielL492@users.noreply.github.com>
2024-03-31 15:19:26 -0400

    sd

--- nycopendata-project/src/views/RawData.vue
+++ nycopendata-project/src/views/RawData.vue
@@ -12,7 +12,32 @@
 </template>
 
 <script setup>
+import { RouterLink, RouterView } from 'vue-router'
 
+import { ref, onBeforeMount } from 'vue';
+
+const loaded = ref(false);
+const rawData = ref([]);
+
+onBeforeMount(() => {
+  loaded.value = false;
+  try {
+    getTrees();
+  } catch (error) {
+    console.warn(error);
+  }
+});
+
+async function getTrees() {
+  try {
+    const res = await fetch('https://data.cityofnewyork.us/resource/uvpi-gqnh.json');
+    rawData.value = await res.json();
+    console.log(rawData.value);
+    loaded.value = true;
+  } catch (error) {
+    console.error(error);
+  }
+}
 </script>
 
 <style lang="scss" scoped>
